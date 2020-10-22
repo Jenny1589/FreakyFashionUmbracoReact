6b6f2c91a0f429731e69f2c8c073e6a00763da13
Jenny <jenny.kallerup@outlook.com>
2020-10-22 20:09:46 +0200

    Fix CORS issue

---
+++ FreakyFashionUmbraco.API/App_Start/WebApiCorsComposer.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace FreakyFashionUmbraco.API.App_Start
+{
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    internal class WebApiCorsComposer : IComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Insert<WebApiCorsComponent>();
+        }
+
+        public class WebApiCorsComponent : IComponent
+        {
+            public void Initialize()
+            {
+                var cors = new EnableCorsAttribute(origins: "*",
+                   headers: "Origin, X-Requested-With, Accept, Authorization, Content-Type",
+                   methods: "GET, POST, PUT, DELETE, OPTIONS");
+
+                GlobalConfiguration.Configuration.EnableCors(cors);
+            }
+
+            public void Terminate()
+            {
+            }
+        }
+    }
+}
