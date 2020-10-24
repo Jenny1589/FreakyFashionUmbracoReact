d5399046b9c8ac7a1eaf00c96715b7f6f91f134b
Jenny <jenny.kallerup@outlook.com>
2020-10-24 20:27:23 +0200

    Add static reference to API URL

--- FreakyFashionUmbraco.API/App_Start/WebApiConfig.cs
+++ FreakyFashionUmbraco.API/App_Start/WebApiConfig.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Web.Http;
 
 namespace FreakyFashionUmbraco.API
 {
     public static class WebApiConfig
     {
+        public static string Url => "https://localhost44348/";
+
         public static void Register(HttpConfiguration config)
         {
             // Web API routes
