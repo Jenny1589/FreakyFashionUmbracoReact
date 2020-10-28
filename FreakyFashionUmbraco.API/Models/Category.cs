e75c2f595a49a805b97700e717eb5e3da8153a53
Jenny <jenny.kallerup@outlook.com>
2020-10-28 21:07:28 +0100

    Make it possible to include products in the category

--- FreakyFashionUmbraco.API/Models/Category.cs
+++ FreakyFashionUmbraco.API/Models/Category.cs
@@ -1,9 +1,24 @@
-﻿namespace FreakyFashionUmbraco.API.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Mapping;
+using Umbraco.Web;
+using Umbraco.Web.PublishedModels;
+
+namespace FreakyFashionUmbraco.API.Models
 {
     public class Category
     {
         public string Name { get; set; }
         public string ImageUrl { get; set; }
         public string Url { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+
+        public void IncludeProducts(UmbracoHelper umbraco, UmbracoMapper mapper)
+        {
+            Products = umbraco.ContentAtRoot().DescendantsOrSelf<ProductPage>()
+                .Where(p => p.Categories.Any(c => c.Name == Name))
+                .Select(p => mapper.Map<Product>(p));
+        }
     }
 }
