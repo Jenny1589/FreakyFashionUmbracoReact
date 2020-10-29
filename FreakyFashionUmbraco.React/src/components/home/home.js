e2558ce450628f2dd9fee70fe0715ecdbc05d8eb
Jenny <jenny.kallerup@outlook.com>
2020-10-29 12:05:57 +0100

    Remove react compile warnings

--- FreakyFashionUmbraco.React/src/components/home/home.js
+++ FreakyFashionUmbraco.React/src/components/home/home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { services } from '../../../package.json';
 import 'bootstrap/dist/js/bootstrap';
 import getContent from '../../hooks/getContent'
--- FreakyFashionUmbraco.React/src/hooks/getContent.js
+++ FreakyFashionUmbraco.React/src/hooks/getContent.js
@@ -4,14 +4,14 @@ export default function(url) {
     const [data, setData] = useState({});
     const [isLoading, setIsLoading] = useState(true);
 
-    async function fetchContent(){
-        const json = await fetch(url).then(response => response.json());
-
-        setData(json);
-        setIsLoading(false);        
-    }
-
     useEffect(() => {
+        async function fetchContent(){
+            const json = await fetch(url).then(response => response.json());
+    
+            setData(json);
+            setIsLoading(false);        
+        }
+        
         fetchContent();
     }, [url]);
 
