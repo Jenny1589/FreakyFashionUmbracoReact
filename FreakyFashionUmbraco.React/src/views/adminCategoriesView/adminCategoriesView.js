0b259cde59cb98bed50f4fa97e9af17b80d308b4
Jenny <jenny.kallerup@outlook.com>
2020-11-22 15:19:51 +0100

    Use table component in products and category views

--- FreakyFashionUmbraco.React/src/views/adminCategoriesView/adminCategoriesView.js
+++ FreakyFashionUmbraco.React/src/views/adminCategoriesView/adminCategoriesView.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Table from '../../components/table/table';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 const AdminCategoriesView = () => {
+    function Category(){
+        this.name = "";
+        this.productCount = "";
+    }
+
+    const {url} = useRouteMatch();
+
     return ( 
-        <h1>Categories</h1>
+        <React.Fragment>
+            <div className="d-flex justify-content-between align-items-center">
+                <h1>Categories</h1>
+                <Link to={`${url}/new`} className="btn btn-primary ml-auto">
+                    New category
+                </Link>
+            </div>
+            <Table items={[new Category()]} />
+        </React.Fragment>        
      );
 }
  
--- FreakyFashionUmbraco.React/src/views/adminProductsView/adminProductsView.js
+++ FreakyFashionUmbraco.React/src/views/adminProductsView/adminProductsView.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import SearchForm from '../../components/searchForm/searchForm';
+import Table from '../../components/table/table';
 
 const AdminProductsView = () => {
-    return ( 
-        <h1>Products</h1>
+    function Product() {
+        this.articleNumber = "";
+        this.productName = "";
+        this.categories = "";
+        this.price = "";
+    }
+
+    const {url} = useRouteMatch();
+
+    return (
+        <React.Fragment>
+            <h1>Products</h1>
+            <div className="d-flex justify-content-between">
+                <SearchForm />
+                <Link to={`${url}/new`} className="btn btn-primary ml-auto">
+                    New product
+                </Link>
+            </div>
+            
+            <Table items={[new Product()]} />
+        </React.Fragment>        
      );
 }
  
