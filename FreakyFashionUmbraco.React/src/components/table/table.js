94b6994355833d413c5681d1eca8c60f980e067e
Jenny <jenny.kallerup@outlook.com>
2020-11-22 19:44:43 +0100

    Fix styling and warnings for table component
    Added key prop and capitalized table headers

--- FreakyFashionUmbraco.React/src/components/table/table.js
+++ FreakyFashionUmbraco.React/src/components/table/table.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './table.css';
 
 const Table = (props) => {
     const { items } = props;
 
-    const renderHeaders = () => Object.keys(items[0]).map(k => <th scope="col">{k}</th>);
-    const renderData = (item) => Object.values(item).map(d => <td>{d}</td>);
-    const renderItems = () => items.map(i => <tr>{renderData(i)}</tr>);
+    const renderHeaders = () => Object.keys(items[0]).map((key, i) => <th key={i} scope="col">{key}</th>);
+    const renderData = (item) => Object.values(item).map((data, i) => <td key={i}>{data}</td>);
+    const renderItems = () => items.map((item, i) => <tr key={i}>{renderData(item)}</tr>);
 
     return (  
         <table className="table table-striped w-100 mt-5">
