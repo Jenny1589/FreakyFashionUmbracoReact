13f52451d4d185262a0324a37f4101ca9dd37afd
Jenny <jenny.kallerup@outlook.com>
2020-11-27 14:20:31 +0100

    Make product form work for edits as well

--- FreakyFashionUmbraco.React/src/components/adminProductForm/adminProductForm.js
+++ FreakyFashionUmbraco.React/src/components/adminProductForm/adminProductForm.js
@@ -1,17 +1,21 @@
 import React, {useState} from 'react';
+import { useParams } from 'react-router-dom';
 import {services} from '../../../package.json';
 import getContent from '../../hooks/getContent';
 
-const AdminProductForm = () => {
+const AdminProductForm = () => {   
+    const {productId} = useParams();
     const getCategoriesUrl = services.ApiUrl + services.contentRoute + 'getallcategories';
+    const getProductUrl = services.ApiUrl + services.contentRoute + 'getproduct?key=' + productId;
     const postUrl = services.ApiUrl + services.contentRoute + 'postproduct'; 
     
     const [message, setMessage] = useState('');
     const [categories, categoriesIsLoading] = getContent(getCategoriesUrl);
+    const [product, productIsLoading] = productId === undefined ? [{categories: []}, false] : getContent(getProductUrl);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const formEl = event.target
+        const formEl = event.target;
         const data = new FormData(formEl);
         
         data.set('categories', [...formEl.querySelectorAll('input[type="checkbox"]')]
@@ -27,7 +31,8 @@ const AdminProductForm = () => {
             method: 'POST',
             body: data
         })
-        .then();
+        .then(res => res.json())
+        .then(json => window.location = `.?productId=${json}`);
     }
 
     function renderMessage(){
@@ -39,10 +44,10 @@ const AdminProductForm = () => {
     }
 
     function renderCategorySelections() {
-        return ( categoriesIsLoading ? <div>Loading...</div> :
+        return ( categoriesIsLoading || productIsLoading ? <div>Loading...</div> :
             categories.map((c, i) => (
                 <div key={i}>
-                    <input id={`${c.name}Selector`} type="checkbox" className="mr-1" value={c.name}/>
+                    <input id={`${c.name}Selector`} type="checkbox" className="mr-1" value={c.name} checked={product.categories.includes(c.name)}/>
                     <label htmlFor={`${c.name}Selector`}>{c.name}</label> 
                 </div>
             ))
@@ -68,13 +73,13 @@ const AdminProductForm = () => {
                 <div className="d-flex align-items-center">
                     <fieldset className="d-flex flex-column p-4 w-50">
                         <label htmlFor="articleNumberInput">Article number</label>
-                        <input id="articleNumberInput" name="articleNumber" type="text" />
+                        <input id="articleNumberInput" name="articleNumber" type="text" value={product.articleNumber} />
 
                         <label htmlFor="productNameInput">Product name</label>
-                        <input id="productNameInput" name="name" type="text" />
+                        <input id="productNameInput" name="name" type="text" value={product.name} />
 
                         <label htmlFor="productDescriptionInput">Description</label>
-                        <textarea id="productDescriptionInput" name="description" rows="5" />
+                        <textarea id="productDescriptionInput" name="description" rows="5" value={product.description} />
 
                         <label htmlFor="productImagesInput">Upload images</label>
                         <input id="productImagesInput" name="images" type="file" accept="image/*" multiple />
@@ -82,12 +87,12 @@ const AdminProductForm = () => {
                         <div className="d-flex justify-content-between">
                             <div className="d-flex flex-column w-50 mr-4">
                                 <label htmlFor="recommendedPriceInput">Recommended price</label>
-                                <input id="recommendedPriceInput" name="recommendedPrice" type="number" step="0.05" />
+                                <input id="recommendedPriceInput" name="recommendedPrice" type="number" step="0.05" value={product.recommendedPrice} />
                             </div>
                             
                             <div className="d-flex flex-column w-50">
                                 <label htmlFor="productPriceInput">Price</label>
-                                <input id="productPriceInput" name="price" type="number" step="0.05" />
+                                <input id="productPriceInput" name="price" type="number" step="0.05" value={product.price} />
                             </div>                    
                         </div> 
                     </fieldset>
